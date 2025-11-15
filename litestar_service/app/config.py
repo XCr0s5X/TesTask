0e5bdffe1e3fae34be6ce3b696a56fbf3a177cc2
Cr0s5 Sans <anonymoushacker006@gmail.com>
2025-11-15 13:45:28 +0200

    Final commit

--- litestar_service/app/config.py
+++ litestar_service/app/config.py
@@ -2,11 +2,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite+aiosqlite:///./offers.db"
+    database_url: str
     debug: bool = True
+    env: str | None = None
 
     class Config:
-        env_file = ".env"
+        env_file = "../.env"  # відносно робочої директорії при запуску через docker-compose
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
